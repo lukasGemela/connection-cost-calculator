36c56275ea5543dfaa76916167256e4f6b2c5b85
Lukas Gemela <lukas.gemela@gfk.com>
2021-10-31 20:55:44 +0100

    code review comments

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 project_dir="${0%/*}"
 
-"${project_dir}"/gradlew clean test bootJar -p ${project_dir}
+"${project_dir}"/gradlew clean test build -p ${project_dir}
 docker build -t flixbus/cost-calculator:latest -f "${project_dir}"/Dockerfile ${project_dir}
